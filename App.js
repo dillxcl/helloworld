35ff9a6e0c8d0f8a7aae5daa739839aeb0f5ae1a
0429charlie <0429charlie@gmail.com>
2020-05-12 16:28:00 -0700

    Chralie's update

--- App.js
+++ App.js
@@ -25,6 +25,7 @@ export default class App extends Component{
         <View style={styles.container}>
           <Text style={styles.welcome}>
             Hello World!
+            Hello Again!!
           </Text>
         </View>
 
