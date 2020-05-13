fb756db5321c7a910ea351699eef7db72e97e49b
0429charlie <0429charlie@gmail.com>
2020-05-12 20:44:55 -0700

    Commit by Charlie

--- App.js
+++ App.js
@@ -7,28 +7,21 @@
  */
 
 import React, {Component} from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import {Platform, StyleSheet, Text, View} from 'react-native';
 
 const instruction = Platform.select({
   ios: 'Press Cmd+R to reload, \n' + 'Cmd+D or shake for dev menu',
-  android: "Double tap R on your keyboard to relaod, \n"+ 'Shake or press menu button for dev menu',
+  android:
+    'Double tap R on your keyboard to relaod, \n' +
+    'Shake or press menu button for dev menu',
 });
 
 export default class App extends Component{
   render(){
     return (
-        <View style={styles.container}>
-          <Text style={styles.welcome}>
-            Hello World!
-            Hello Again!!
-          </Text>
-        </View>
-
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Hello World by Charlie!</Text>
+      </View>
     );
   }
 }
@@ -51,4 +44,3 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
 });
-
